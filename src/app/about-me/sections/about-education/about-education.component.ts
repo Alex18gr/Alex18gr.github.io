4360d9e23136ee271e60a055143605eeeaaaa680
alexc <alexchih02@gmail.com>
2024-10-18 23:22:49 +0300

    Added education items

--- src/app/about-me/sections/about-education/about-education.component.ts
+++ src/app/about-me/sections/about-education/about-education.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, signal} from '@angular/core';
+import {EducationItemComponent} from './education-item/education-item.component';
+import {educationItems} from './education-items/education-items';
 
 @Component({
   selector: 'app-about-education',
   standalone: true,
-  imports: [],
+  imports: [
+    EducationItemComponent
+  ],
   templateUrl: './about-education.component.html',
   styleUrl: './about-education.component.scss'
 })
 export class AboutEducationComponent {
-
+  educationItems = signal(educationItems);
 }
