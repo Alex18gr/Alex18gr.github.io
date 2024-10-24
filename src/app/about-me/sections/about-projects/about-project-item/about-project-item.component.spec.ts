73322abb50a6971855fa79c3e0fd6cbcd5f0b6b9
alexc <alexchih02@gmail.com>
2024-10-25 01:10:53 +0300

    Fixed test

--- src/app/about-me/sections/about-projects/about-project-item/about-project-item.component.spec.ts
+++ src/app/about-me/sections/about-projects/about-project-item/about-project-item.component.spec.ts
@@ -5,6 +5,15 @@ import { AboutProjectItemComponent } from './about-project-item.component';
 describe('AboutProjectItemComponent', () => {
   let component: AboutProjectItemComponent;
   let fixture: ComponentFixture<AboutProjectItemComponent>;
+  const testProject = {
+    title: 'IDE Learn',
+    descriptionContent: '',
+    duration: {
+      from: new Date('2023-07-01'),
+      to: new Date('2023-07-01')
+    },
+    tags: []
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -14,6 +23,7 @@ describe('AboutProjectItemComponent', () => {
 
     fixture = TestBed.createComponent(AboutProjectItemComponent);
     component = fixture.componentInstance;
+    fixture.componentRef.setInput('project', testProject);
     fixture.detectChanges();
   });
 
